8a2af4734efb4eaf464af8bb4644380fbadbc7d8
Ale <alegonzalez1993@gmail.com>
2022-06-05 23:10:23 -0400

    First commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 mb-8"
+  >
+    <ul class="flex flex-wrap -mb-px">
+      <li class="mr-2">
+        <router-link
+          to="/"
+          class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-blue-600 hover:border-blue-600"
+          >Home</router-link
+        >
+      </li>
+      <li class="mr-2">
+        <router-link
+          to="/juegos"
+          class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-blue-600 hover:border-blue-600"
+          >Juegos</router-link
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home container mx-auto">
+    <h1 class="mx-auto mb-5 text-blue-800 text-center">Tienda de juegos 32 bits</h1>
+    <img
+      alt="Vue logo"
+      class="mx-auto"
+      src="https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+  components: {},
+}
+</script>
